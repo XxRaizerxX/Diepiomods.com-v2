758c0b1c2614d0d5edbeaadce722863f00c6fca0
eralpcicekdagi <eralpcicekdagi@hotmail.com>
2016-05-25 08:56:53 +0300

    Add files via upload

---
+++ version2.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         Diep.io Mods by Diepiomods.com v2.0
+// @version      2.0
+// @description  Diep.io (zoom, minimap, autshoot[E])
+// @author       Diepiomods.com
+// @match        http://diep.io
+// @run-at       document-start
+// @grant        GM_getResourceText
+// @resource html http://agarw.com/diepmodu20.html
+// @namespace Diepiomods.com
+// ==/UserScript==  
+
+window.stop(); 
+document.open(), document.write(GM_getResourceText("html")), document.close();
